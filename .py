00fb6ac01fa3d7a10f81772017c87f655cdf76c5
Gm <omer.sharif1998@gmail.com>
2025-02-09 20:47:58 +0300

    Update .py

--- .py
+++ .py
@@ -1,14 +1,20 @@
 import os, shutil
 
-path = r"C:\Users\omers\Downloads\Python" #replace this path with the path of the folder you want to organize
-os.listdir(path) 
-folder_names = ['Images', 'Python files', 'Text files', 'PDF files', 'Word files', 'Excel files', 'Powerpoint files', 'CSV files']
-
-for loop in range(0,2):
-   if os.path.exists(path +folder_names[loop]):
-        print(path +folder_names[loop])
-        os.makedirs(path +folder_names[loop])
+path = r"C:\Users\omers\Downloimport os" #replace with your path destination
+import shutil
+import tkinter as tk
+from tkinter import filedialog
 
 
+def sort_files():
+    folder_path = filedialog.askdirectory()
+    if not folder_path:
+        return
 
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            file_extension = filename.split('.')[-1]
+            destination_folder = os.path.join(folder_path, file_extension)
 
+          
