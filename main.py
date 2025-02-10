44550215eac44d4261c9e84751eceb35ac089394
Gm <omer.sharif1998@gmail.com>
2025-02-10 20:42:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ def sort_files():
         file_path = os.path.join(folder_path, filename)
         if os.path.isfile(file_path):
             file_extension = filename.split('.')[-1]
-            destination_folder = os.path.join(folder_path, file_extension)
+            destination_folder = os.path.join(folder_path, file_import os
 
-          
+
+            if not os.path.exists(destination_folder):
+                os.makedirs(destination_folder)
+
+            shutil.move(file_path, os.path.join(destination_folder, filename))
+
+# Set up the GUI
+root = tk.Tk()
+root.title('File Sorter')
+
+sort_button = tk.Button(root, text='Sort Files', command=sort_files
